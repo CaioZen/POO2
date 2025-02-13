b2c30cc68c1a590e318145d858860e8a174ab925
CaioZen <caiozengoncalves@gmail.com>
2025-02-13 14:24:18 -0300

    init

---
+++ src/main/java/com/caiozen/trabalhopoo2/TrabalhoPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.caiozen.trabalhopoo2;
+
+/**
+ *
+ * @author 2023122760123
+ */
+public class TrabalhoPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
