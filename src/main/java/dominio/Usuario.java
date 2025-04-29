197058ff3b2a5629c58eb5a98504c3fe61b57f72
CaioZen <caiozengoncalves@gmail.com>
2025-04-29 14:30:18 -0300

    Hibernate inicio

--- src/main/java/dominio/Usuario.java
+++ src/main/java/dominio/Usuario.java
@@ -1,22 +1,39 @@
 package dominio;
 
+import java.io.Serializable;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-public class Usuario {
-
+@NoArgsConstructor
+@Entity
+public class Usuario implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idUsr;
+    @Column
     private String nome;
+    @Column 
     private String celular;
+    @Column
     private String email;
+    @Column
     private String cep;
+    @Column
     private String cidade;
+    @Column (length = 2)
     private String uf;
+    @Column 
     private int numeroCasa;
+    @Column 
     private String rua;
+    @Column 
     private String bairro;
+    @Column 
     private String referencia;
 
     public Usuario(String nome, String celular, String email, String cep, String cidade, String uf, int numeroCasa, String rua, String bairro, String referencia) {
